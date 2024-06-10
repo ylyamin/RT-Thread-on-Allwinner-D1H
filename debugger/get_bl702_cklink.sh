5c8122ead2e388f799243e43bc9442acc9feef5d
Yury Lyamin <lyamin@arrival.com>
2024-06-10 14:17:08 +0400

    add debugger

---
+++ debugger/get_bl702_cklink.sh
@@ -0,0 +1,1 @@
+wget https://github.com/bouffalolab/bouffalo_sdk/blob/master/tools/cklink_firmware/bl702_cklink_whole_img_v2.2.bin
---
+++ debugger/get_blisp.sh
@@ -0,0 +1,1 @@
+wget https://github.com/pine64/blisp/releases/download/v0.0.4/blisp-linux-x86_64-v0.0.4.zip
