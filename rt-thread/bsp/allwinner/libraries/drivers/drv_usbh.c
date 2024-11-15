0bb9f7f49968e291a99891641ada7dc1edb4727a
Yury Lyamin <lyamin@arrival.com>
2024-11-15 17:08:16 +0400

    remove Chery from config

--- rt-thread/bsp/allwinner/libraries/drivers/drv_usbh.c
+++ rt-thread/bsp/allwinner/libraries/drivers/drv_usbh.c
@@ -1,17 +1,15 @@
 #include <stdio.h>
 #include <kconfig.h>
-//#include <usb_os_platform.h>
 #include <hal_log.h>
 #include <hal_cmd.h>
 #include <rtthread.h>
 #include <rtdevice.h>
-//#include <usbh_core.h> //CherryUSB
 
 extern void _axp_USB_control(int on);
 
 int drv_usb_hal(void)
 {
-    _axp_USB_control(1);
+    //_axp_USB_control(1);
     //hal_udc_init();
     //hal_usb_core_init();
 	//hal_usb_hcd_init(0);
@@ -23,22 +21,12 @@ int drv_usb_hal(void)
 MSH_CMD_EXPORT_ALIAS(drv_usb_hal, usb_hal, usb);
 //INIT_DEVICE_EXPORT(drv_usb_hal);
 
-int drv_usb_cherry(void)
-{
-    _axp_USB_control(1);
-    //usbh_initialize(1, 0x04200000);
-    //usbh_initialize(0, 0x04101000);
-    //usbh_initialize(2, 0x04100000);
-}
-MSH_CMD_EXPORT_ALIAS(drv_usb_cherry, usb_cherry, usb);
-//INIT_DEVICE_EXPORT(drv_usb_cherry);
-
 extern int init_tinyusb(void);
 
 int drv_usb_tiny(void)
 {
     _axp_USB_control(1);
-    init_tinyusb();
+    //init_tinyusb();
 }
 MSH_CMD_EXPORT_ALIAS(drv_usb_tiny, usb_tiny, usb);
 //INIT_DEVICE_EXPORT(drv_usb_tiny);
